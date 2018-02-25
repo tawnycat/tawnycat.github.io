31018179e5721522fff27f31a8da09b4211b5f54
tawnycat <tawnyeleanor@gmail.com>
2018-02-25 18:45:26 -0500

    added animations to about

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,14 @@
-    $(document).ready(function() {
-        $(".button-collapse").sideNav();
+$(document).ready(function() {
+
+    $(".button-collapse").sideNav();
+
+    let time = 1000;
+
+    $(".dev-icons").get().forEach(function(icon) {
+    	$(icon).hide();
+        setTimeout(function() {
+            $(icon).show().addClass('animated zoomIn');
+        }, time);
+        time += 250; 
     });
+});
